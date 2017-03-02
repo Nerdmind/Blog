ac66d6889e6b5b19130754341c2736cfca301549
Thomas Lange <code@nerdmind.de>
2017-03-02 13:09:44 +0100

    Unnecessary line breaks removed.

--- template/standard/html/post/item.php
+++ template/standard/html/post/item.php
@@ -10,7 +10,7 @@
 <li class="item-list-li post">
 	<header>
 		<h2>
-			<a href="<?=$POST['URL']?>"><?=escapeHTML($POST['ATTR']['NAME'])."\n"?></a>
+			<a href="<?=$POST['URL']?>"><?=escapeHTML($POST['ATTR']['NAME'])?></a>
 			<time class="brackets info" datetime="<?=$POST['ATTR']['TIME_INSERT']?>"><?=parseDatetime($POST['ATTR']['TIME_INSERT'], $Language->template('date_format'))?></time>
 		</h2>
 	</header>
