1e49615c8c433c1f9c2f2ade1762411f2f3eb091
Thomas Lange <code@nerdmind.de>
2017-03-04 22:19:02 +0100

    Factory method "buildBySlug" is now defined in the "ItemFactory" class.

--- core/namespace/Page/Factory.php
+++ core/namespace/Page/Factory.php
@@ -1,9 +1,5 @@
 <?php
 namespace Page;
 
-class Factory extends \ItemFactory {
-	public static function buildBySlug($slug): \Item {
-		return self::build(Item::getIDByField('slug', $slug, \Application::getDatabase()));
-	}
-}
+class Factory extends \ItemFactory {}
 ?>
--- core/namespace/User/Factory.php
+++ core/namespace/User/Factory.php
@@ -2,10 +2,6 @@
 namespace User;
 
 class Factory extends \ItemFactory {
-	public static function buildBySlug($slug): \Item {
-		return self::build(Item::getIDByField('slug', $slug, \Application::getDatabase()));
-	}
-
 	public static function buildByUsername($username): \Item {
 		return self::build(Item::getIDByField('username', $username, \Application::getDatabase()));
 	}
