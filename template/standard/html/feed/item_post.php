39944454324b4c66b8cf2444cca17c149208dfac
Thomas Lange <code@nerdmind.de>
2017-04-24 17:15:41 +0200

    HTML escaping is required to prevent XML validation errors for some characters like "&".

--- template/standard/html/feed/item_post.php
+++ template/standard/html/feed/item_post.php
@@ -13,11 +13,10 @@
 	<guid isPermaLink="false"><?=$POST['GUID']?></guid>
 	<pubDate><?=parseDatetime($POST['ATTR']['TIME_INSERT'], '[RFC2822]')?></pubDate>
 	<dc:creator><?=escapeHTML($USER['ATTR']['FULLNAME'])?></dc:creator>
-	<description><?=description($POST['BODY']['HTML'], 400)?></description>
+	<description><?=escapeHTML(description($POST['BODY']['HTML'], 400))?></description>
 	<content:encoded>
 		<![CDATA[
 			<?=$POST['BODY']['HTML']?>
-			<p><small><strong>Kommentare:</strong> [<a href="https://keybase.io/nerdmind">0x33EB32A2</a>] blog&#64;nerdmind.de</small></p>
 		]]>
 	</content:encoded>
 	<?php foreach($POST['FILE']['LIST'] as $fileURL): ?>
