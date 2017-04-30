77b3a519041f99275563e7609c0ff619e9e935e0
Thomas Lange <code@nerdmind.de>
2017-04-30 18:07:32 +0200

    Missing internationalization for feed link titles added.

--- template/standard/html/home.php
+++ template/standard/html/home.php
@@ -7,7 +7,7 @@
 #                                                                              #
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%#
 ?>
-<h1><i class="fa fa-home"></i><?=$Language->template('home_heading_text', escapeHTML(Application::get('BLOGMETA.NAME')))?><span class="head-link brackets"><i class="fa fa-rss"></i><a href="<?=Application::getURL('feed/')?>" title="Alle Inhalte">Feed</a></span></h1>
+<h1><i class="fa fa-home"></i><?=$Language->template('home_heading_text', escapeHTML(Application::get('BLOGMETA.NAME')))?><span class="head-link brackets"><i class="fa fa-rss"></i><a href="<?=Application::getURL('feed/')?>" title="<?=$Language->text('feed_name_items', escapeHTML($BLOGMETA['NAME']))?>">Feed</a></span></h1>
 <p><?=$Language->template('home_heading_desc', Application::get('POST.LIST_SIZE'))?></p>
 
 <ul class="item-list post">
