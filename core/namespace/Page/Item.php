ea29b80ec2942f7e0390e3bb98c37ac6c4c5686d
Thomas Lange <code@nerdmind.de>
2018-04-01 17:10:21 +0200

    Remove unused method "getSearchResultIDs" from Page and Post class

--- core/namespace/Page/Item.php
+++ core/namespace/Page/Item.php
@@ -27,20 +27,6 @@ class Item extends \Item {
 	}
 
 	#===============================================================================
-	# Return unique page IDs for search results
-	#===============================================================================
-	public static function getSearchResultIDs($search, \Database $Database): array {
-		$Statement = $Database->prepare(sprintf("SELECT id FROM %s WHERE
-			MATCH(name, body) AGAINST(? IN BOOLEAN MODE) LIMIT 20", Attribute::TABLE));
-
-		if($Statement->execute([$search])) {
-			return $Statement->fetchAll($Database::FETCH_COLUMN);
-		}
-
-		return [];
-	}
-
-	#===============================================================================
 	# Return search results as Page\Attribute
 	#===============================================================================
 	public static function getSearchResults($search, \Database $Database): array {
