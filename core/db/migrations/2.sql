fefa367242de85f46250fb8da46dbae3f5545836
Thomas Lange <code@nerdmind.de>
2021-06-17 00:18:34 +0200

    Add migrations directory
    This commit adds the directory core/db/migrations/ which contains all
    migrations that were applied to the database since the first release.

---
+++ core/db/migrations/2.sql
@@ -0,0 +1,3 @@
+ALTER TABLE `page` ADD UNIQUE KEY `time_insert` (`time_insert`);
+ALTER TABLE `post` ADD UNIQUE KEY `time_insert` (`time_insert`);
+ALTER TABLE `user` ADD UNIQUE KEY `time_insert` (`time_insert`);
