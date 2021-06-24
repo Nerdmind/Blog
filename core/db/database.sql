55ae320e7cfd710f3ea0f295c880619217db2220
Thomas Lange <code@nerdmind.de>
2021-06-24 18:06:03 +0200

    Remove migration for migration table (readme)
    This commit removes the migration number 5 which created the migration
    table. The initialization of the migration table will be implemented in
    the upcoming migration script by itself. Sorry!
    
    If you recently installed the application with a fresh database which
    contained the migration table, please manually set the schema_version
    to 4. It is recommended to only install a specific tag instead of the
    latest master branch unless you are a developer or know what you do.
    
    UPDATE migration SET schema_version = 4

--- core/db/database.sql
+++ core/db/database.sql
@@ -3,7 +3,7 @@
 -- =============================================================================
 CREATE TABLE `migration` (`schema_version` smallint(4) NOT NULL)
 	ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-INSERT INTO `migration` (`schema_version`) VALUES (5);
+INSERT INTO `migration` (`schema_version`) VALUES (4);
 
 -- =============================================================================
 -- Table structure for page items
--- core/db/migrations/5.sql
+++
@@ -1,3 +0,0 @@
-CREATE TABLE `migration` (`schema_version` smallint(4) NOT NULL)
-	ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-INSERT INTO `migration` (`schema_version`) VALUES (5);
