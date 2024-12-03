c594f2346bba05118b9ccc29241f85b6525821dd
Thomas Lange <code@nerdmind.de>
2024-12-03 20:29:02 +0100

    Bugfix: Date in search form not kept after submit
    Use the `==` (equal) instead of `===` (identical) comparison operator in
    the form template. This bug was silently introduced in 2021 with 8cd1105
    in `core/include/search/main.php` where the `$FORM['OPTIONS']` array is
    since then filled with integer values and not string values anymore.

--- theme/default/html/search/main.php
+++ theme/default/html/search/main.php
@@ -12,21 +12,21 @@
 		<option value=""><?=$Language->text('date_d')?></option>
 
 		<?php foreach($FORM['OPTIONS']['D'] as $option): ?>
-			<option value="<?=$option?>"<?=($FORM['SELECT']['D'] === $option) ? ' selected' : '' ?>><?=$option?></option>
+			<option value="<?=$option?>"<?=($FORM['SELECT']['D'] == $option) ? ' selected' : '' ?>><?=$option?></option>
 		<?php endforeach; ?>
 	</select>
 	<select name="m">
 		<option value=""><?=$Language->text('date_m')?></option>
 
 		<?php foreach($FORM['OPTIONS']['M'] as $option): ?>
-			<option value="<?=$option?>"<?=($FORM['SELECT']['M'] === $option) ? ' selected' : '' ?>><?=$option?></option>
+			<option value="<?=$option?>"<?=($FORM['SELECT']['M'] == $option) ? ' selected' : '' ?>><?=$option?></option>
 		<?php endforeach; ?>
 	</select>
 	<select name="y">
 		<option value=""><?=$Language->text('date_y')?></option>
 
 		<?php foreach($FORM['OPTIONS']['Y'] as $option): ?>
-			<option value="<?=$option?>"<?=($FORM['SELECT']['Y'] === $option) ? ' selected' : '' ?>><?=$option?></option>
+			<option value="<?=$option?>"<?=($FORM['SELECT']['Y'] == $option) ? ' selected' : '' ?>><?=$option?></option>
 		<?php endforeach; ?>
 	</select>
 
